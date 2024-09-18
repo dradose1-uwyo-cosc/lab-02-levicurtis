b8dcebfe38b50d096a24f749c19425b4fbc4e717
levicurtis <lcurtis9@uwyo.edu>
2024-09-18 10:09:31 -0600

    Update cosc1010-lab02.py

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,6 +1,6 @@
-# Your Name Here
+# Levi Curtis
 # UWYO COSC 1010
-# Submission Date
+# September 19, 2024
 # Lab 02 
 # Lab Section: 
 # Sources, people worked with, help given to: 
@@ -13,14 +13,16 @@ your_variable_here = "when you see this, replace it with your code"
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print( "Hello, COSC1010" )
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "Hello, COSC1010"
+print("hello_message")
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+yee_haw = "cowboy joe"
+
+print("yee_haw")
 
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
